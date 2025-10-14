97dc1c1acf4b319cbd367851165f72d6289ed3bc
00ill <cyi1769@naver.com>
2025-10-14 19:55:26 +0900

    Practice Character Device

--- 00_practice/clone_dd_hello.c
+++ 00_practice/clone_dd_hello.c
@@ -1,32 +1,29 @@
-// SPDX-License-Identifier: GPL-2.0
-// Simple "Hello, Kernel!" Character Device Driver
-
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/init.h>
+#include "linux/cdev.h"
+#include "linux/device.h"
+#include "linux/init.h"
+#include "linux/kern_levels.h"
+#include "linux/module.h"
+#include "linux/sysctl.h"
+#include "linux/types.h"
 #include <linux/cdev.h>
-#include <linux/device.h>
+#include <linux/fs.h>
 
 #define DRIVER_NAME "myhello"
 #define NUM_DEVICES 1
 
-static dev_t device_num;
-static struct cdev cdev;
-static struct class *myhello_class;
-
-static int myhello_open(struct inode *inode, struct file *file)
+static int device_open(struct inode* inode, struct file* file)
 {
-    printk(KERN_INFO "myhello: device opened\n");
+    printk(KERN_INFO "my device OPEN\n");
     return 0;
 }
 
-static int myhello_release(struct inode *inode, struct file *file)
+static int device_release(struct inode* inode, struct file* file)
 {
-    printk(KERN_INFO "myhello: device closed\n");
+    printk(KERN_INFO "my device RELEASE\n");
     return 0;
 }
 
-static ssize_t myhello_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+static ssize_t device_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
 {
     char msg[] = "Hello, Kernel!\n";
     size_t len = sizeof(msg);
@@ -41,34 +38,52 @@ static ssize_t myhello_read(struct file *file, char __user *buf, size_t count, l
     return count;
 }
 
-static struct file_operations myhello_fops = {
+/* Struct for Device Driver
+ * In linux/fs.h
+ */
+static struct file_operations my_char_fops = {
     .owner = THIS_MODULE,
-    .open = myhello_open,
-    .release = myhello_release,
-    .read = myhello_read,
+    .open = device_open,
+    .release = device_release,
+    .read = device_read,
 };
+
+/* dev_t -> U32 type
+ * In linux/types.h
+ * Device Number
+ */
+static dev_t device_num;
+static struct cdev cdev;
+/* In class.h
+ *
+ */
+static struct class* myhello_class;
 
 static int __init myhello_init(void)
 {
     int ret;
-    // 1. Allocate device number
+    /* int alloc_chrdev_region(dev_t *dev, unsigned int firstminor, unsigned int count, char *name)
+     * character device driver API using dynamic allocate
+     */
+    //
     ret = alloc_chrdev_region(&device_num, 0, NUM_DEVICES, DRIVER_NAME);
-    if (ret < 0) {
-        printk(KERN_ERR "myhello: failed to allocate device number\n");
+    if (ret < 0)
+    {
+        printk(KERN_ERR "myhello : failed to allocate device number\n");
         return ret;
     }
-    // 2. Initialize and add character device
-    cdev_init(&cdev, &myhello_fops);
-    cdev.owner = THIS_MODULE;
+    cdev_init(&cdev, &my_char_fops);
     ret = cdev_add(&cdev, device_num, NUM_DEVICES);
-    if (ret < 0) {
+    if (ret < 0)
+    {
         unregister_chrdev_region(device_num, NUM_DEVICES);
         printk(KERN_ERR "myhello: failed to add cdev\n");
         return ret;
     }
-    // 3. Create device node
-    myhello_class = class_create(THIS_MODULE, DRIVER_NAME);
-    if (IS_ERR(myhello_class)) {
+
+    myhello_class = class_create(DRIVER_NAME);
+    if (IS_ERR(myhello_class))
+    {
         cdev_del(&cdev);
         unregister_chrdev_region(device_num, NUM_DEVICES);
         printk(KERN_ERR "myhello: failed to create class\n");
